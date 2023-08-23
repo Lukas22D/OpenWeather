69305ff7cc601a0e3574d318f55836e12118aea3
Lukas22D <luk.look.s.rocha@gmail.com>
2023-08-23 13:05:33 -0300

    atualização do background interativo

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 // Variables
-import chave from './senhas.js';
+import {chavePixels, chave} from './senhas.js';
 const apiCountryURL = "https://www.countryflagicons.com/FLAT/64/";
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
 const APIKEY = chave;
-const apikey2 ="Q2V4GlrngVlT5o2EiBZ4NMcdOvF3cVBplITgAYK5Z3ndd63VELk7Gjiy";
 
 const cityElement = document.querySelector("#city");
 const tempElement = document.querySelector("#temperature span");
@@ -19,30 +18,56 @@ const weatherContainer = document.querySelector("#weather-data");
 //Functions
 const getWeatherData = async (city) => {
     const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${APIKEY}&units=metric&lang=pt_br`;
-    
+
     const res = await fetch(apiWeatherURL);
     const data = await res.json();
 
     return data;
 };
 
- const showWeatherData = async (city) => {
+function getBackgroundImage(data, apiKey) {
+    // Mudança do fundo 
+    const body = document.querySelector('body');
+    const perPage = 1;
+
+    fetch(`https://api.pexels.com/v1/search?query=${data}&per_page=${perPage}`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `${apiKey}`
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            const photos = data.photos;
+            if (photos.length > 0) {
+                const photo = photos[0];
+                body.style.backgroundImage = `url('${photo.src.large2x}')`;
+                console.log(photo);
+            } else {
+                console.log('No photos found.');
+            }
+        })
+}
+
+const showWeatherData = async (city) => {
     const data = await getWeatherData(city);
-    if(data.name !== undefined){
-    cityElement.innerText = data.name;
-    tempElement.innerText = parseInt(data.main.temp);
-    descElement.innerText = data.weather[0].description;
-    weatherIconElement.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
-    countryElement.src = `${apiCountryURL}${data.sys.country}.png`;
-    humidityElement.innerText = `${data.main.humidity}%`;
-    windElement.innerText = `${data.wind.speed}km/h`;
-    weatherContainer.classList.remove("hide");
-    }else{
+    if (data.name !== undefined) {
+        cityElement.innerText = data.name;
+        tempElement.innerText = parseInt(data.main.temp);
+        descElement.innerText = data.weather[0].description;
+        weatherIconElement.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+        countryElement.src = `${apiCountryURL}${data.sys.country}.png`;
+        humidityElement.innerText = `${data.main.humidity}%`;
+        windElement.innerText = `${data.wind.speed}km/h`;
+        weatherContainer.classList.remove("hide");
+        getBackgroundImage(data.name, chavePixels);
+
+    } else {
         alert("Cidade não encontrada");
         weatherContainer.classList.add("hide");
-        }
-    
- };
+    }
+
+};
 
 // Event 
 
@@ -53,8 +78,8 @@ searchBtn.addEventListener("click", (e) => {
 });
 
 cityInput.addEventListener("keyup", (e) => {
-    
-    if(e.code === "Enter"){
+
+    if (e.code === "Enter") {
         const city = e.target.value;
         showWeatherData(city);
     }
---
+++ js/senhas.js
@@ -0,0 +1,7 @@
+ // keyimg: Q2V4GlrngVlT5o2EiBZ4NMcdOvF3cVBplITgAYK5Z3ndd63VELk7Gjiy
+ 
+ 
+ const chave= "Insira sua Chave";
+ const chavePixels = "Insira sua Chave"
+
+ export  {chavePixels, chave};
